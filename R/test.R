b9e9547fd637ad710e3ac47ec4965a787a8bc0bb
DavisVaughan <davis@rstudio.com>
2022-04-26 16:52:09 -0400

    Initial commit

---
+++ R/test.R
@@ -0,0 +1,10 @@
+#' Test impl
+#'
+#' This is the implementation that powers [testinherit::test()].
+#'
+#' @param x A value
+#'
+#' @seealso [testinherit::test()]
+test_impl <- function(x) {
+  x
+}
